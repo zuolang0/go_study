3461cf8dea1be5e330e92f35246d6ab9a7dff2ff
langzi <1154505909@qq.com>
2017-07-03 10:39:45 +0800

    go 学习

--- fun/geturl.go
+++ fun/geturl.go
@@ -5,16 +5,18 @@ import "fmt"
 //import "io/ioutil"
 import "net/http"
 import "os"
-
+import "strings"
 import "io"
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if strings.HasPrefix(url, "http://") == false {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch :%v \n", err)
 		}
-		//dst := ""
 		//b, err := ioutil.ReadAll(resp.Body)
 		n, err := io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
@@ -22,7 +24,10 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch :reading %s:%v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", n)
+		/*var ptr *int
+		  ptr = &n*/
+		//fmt.Printf("%s", b)
+		fmt.Printf("%x", ptr)
 	}
 
 }
